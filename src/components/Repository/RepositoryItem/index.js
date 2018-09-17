517804a26b2d769461e6805ddc9dff39c2468aa8
Ryan Yogan <ryanyogan@gmail.com>
2018-09-17 16:07:04 -0700

    added ability to addStar to a repo

--- src/components/Repository/RepositoryItem/index.js
+++ src/components/Repository/RepositoryItem/index.js
@@ -1,11 +1,26 @@
 /* eslint-disable react/no-danger */
 import React from 'react';
+import gql from 'graphql-tag';
+import { Mutation } from 'react-apollo';
 
 import Link from '../../Link';
+import Button from '../../Button';
 
 import '../style.css';
 
+const STAR_REPOSITORY = gql`
+  mutation($id: ID!) {
+    addStar(input: { starrableId: $id }) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`;
+
 const RepositoryItem = ({
+  id,
   name,
   url,
   descriptionHTML,
@@ -22,8 +37,14 @@ const RepositoryItem = ({
         <Link href={url}>{name}</Link>
       </h2>
 
-      <div className="RepositoryItem-title-action">
-        {stargazers.totalCount} Stars
+      <div>
+        <Mutation mutation={STAR_REPOSITORY} variables={{ id }}>
+          {addStar => (
+            <Button className={'RepositoryItem-title-action'} onClick={addStar}>
+              {stargazers.totalCount} Star
+            </Button>
+          )}
+        </Mutation>
       </div>
     </div>
 
