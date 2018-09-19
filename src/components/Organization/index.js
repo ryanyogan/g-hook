3a5f903c61467971d0ebf166ac5c534cdff4aa6f
Ryan Yogan <ryanyogan@gmail.com>
2018-09-19 09:22:42 -0700

    added routing, header, organization screen

---
+++ src/components/Organization/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+import RepositoryList, { REPOSITORY_FRAGMENT } from '../Repository';
+import Loading from '../Loading';
+import ErrorMessage from '../Error';
+
+const GET_REPOSITORIES_OR_ORGANIZATION = gql`
+  query($organizationName: String!, $cursor: String) {
+    organization(login: $organizationName) {
+      repositories(first: 5, after: $cursor) {
+        edges {
+          node {
+            ...repository
+          }
+        }
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+      }
+    }
+  }
+
+  ${REPOSITORY_FRAGMENT}
+`;
+
+const Organization = ({ organizationName }) => (
+  <Query
+    query={GET_REPOSITORIES_OR_ORGANIZATION}
+    variables={{ organizationName }}
+    skip={organizationName === ''}
+    notifyOnNetworkStatusChange
+  >
+    {({ data, loading, error, fetchMore }) => {
+      if (error) {
+        return <ErrorMessage error={error} />;
+      }
+
+      const { organization } = data;
+
+      if (loading && !organization) {
+        return <Loading />;
+      }
+
+      return (
+        <RepositoryList
+          loading={loading}
+          repositories={organization.repositories}
+          fetchMore={fetchMore}
+        />
+      );
+    }}
+  </Query>
+);
+
+export default Organization;
